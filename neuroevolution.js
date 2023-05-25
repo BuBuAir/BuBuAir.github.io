067eeca74d2afbebb3cfb9d0484900accc0ad3eb
BuBuAir <132973357+BuBuAir@users.noreply.github.com>
2023-05-25 12:07:53 +0800

    Update neuroevolution.js

--- neuroevolution.js
+++ neuroevolution.js
@@ -411,3 +411,140 @@ var Neuroevolution = function (options) {
 	/**
 	 * Generations class.
 	 * 
+	 * Hold's previous Generations and current Generation.
+	 *
+	 * @constructor
+	 */
+	var Generations = function () {
+		this.generations = [];
+		var currentGeneration = new Generation();
+	}
+
+	/**
+	 * Create the first generation.
+	 *
+	 * @param {input} Input layer.
+	 * @param {input} Hidden layer(s).
+	 * @param {output} Output layer.
+	 * @return First Generation.
+	 */
+	Generations.prototype.firstGeneration = function (input, hiddens, output) {
+		// FIXME input, hiddens, output unused.
+
+		var out = [];
+		for (var i = 0; i < self.options.population; i++) {
+			// Generate the Network and save it.
+			var nn = new Network();
+			nn.perceptronGeneration(self.options.network[0],
+				self.options.network[1],
+				self.options.network[2]);
+			out.push(nn.getSave());
+		}
+
+		this.generations.push(new Generation());
+		return out;
+	}
+
+	/**
+	 * Create the next Generation.
+	 *
+	 * @return Next Generation.
+	 */
+	Generations.prototype.nextGeneration = function () {
+		if (this.generations.length == 0) {
+			// Need to create first generation.
+			return false;
+		}
+
+		var gen = this.generations[this.generations.length - 1]
+			.generateNextGeneration();
+		this.generations.push(new Generation());
+		return gen;
+	}
+
+	/**
+	 * Add a genome to the Generations.
+	 *
+	 * @param {genome}
+	 * @return False if no Generations to add to.
+	 */
+	Generations.prototype.addGenome = function (genome) {
+		// Can't add to a Generation if there are no Generations.
+		if (this.generations.length == 0) return false;
+
+		// FIXME addGenome returns void.
+		return this.generations[this.generations.length - 1].addGenome(genome);
+	}
+
+
+	/*SELF************************************************************************/
+	self.generations = new Generations();
+
+	/**
+	 * Reset and create a new Generations object.
+	 *
+	 * @return void.
+	 */
+	self.restart = function () {
+		self.generations = new Generations();
+	}
+
+	/**
+	 * Create the next generation.
+	 *
+	 * @return Neural Network array for next Generation.
+	 */
+	self.nextGeneration = function () {
+		var networks = [];
+
+		if (self.generations.generations.length == 0) {
+			// If no Generations, create first.
+			networks = self.generations.firstGeneration();
+		} else {
+			// Otherwise, create next one.
+			networks = self.generations.nextGeneration();
+		}
+
+		// Create Networks from the current Generation.
+		var nns = [];
+		for (var i in networks) {
+			var nn = new Network();
+			nn.setSave(networks[i]);
+			nns.push(nn);
+		}
+
+		if (self.options.lowHistoric) {
+			// Remove old Networks.
+			if (self.generations.generations.length >= 2) {
+				var genomes =
+					self.generations
+					.generations[self.generations.generations.length - 2]
+					.genomes;
+				for (var i in genomes) {
+					delete genomes[i].network;
+				}
+			}
+		}
+
+		if (self.options.historic != -1) {
+			// Remove older generations.
+			if (self.generations.generations.length > self.options.historic + 1) {
+				self.generations.generations.splice(0,
+					self.generations.generations.length - (self.options.historic + 1));
+			}
+		}
+
+		return nns;
+	}
+
+	/**
+	 * Adds a new Genome with specified Neural Network and score.
+	 *
+	 * @param {network} Neural Network.
+	 * @param {score} Score value.
+	 * @return void.
+	 */
+	self.networkScore = function (network, score) {
+		self.generations.addGenome(new Genome(score, network.getSave()));
+	}
+}
